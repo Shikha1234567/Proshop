351c2bd21cf3725695c62252c5e746055cbfa449
gauravgk <gaurav.v.kumar@oracle.com>
2022-02-21 17:09:08 +0530

    proshop uploaded

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+
+
+import productRoutes from './routes/productRoutes.js'
+
+dotenv.config()
+connectDB()
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('API is running.......')
+})
+app.use('/api/products', productRoutes)
+
+const PORT = process.env.PORT || 8080
+app.listen(PORT, console.log(`Server running on port ${PORT}`))
